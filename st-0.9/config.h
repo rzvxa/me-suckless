8d5e9a4b12733df5e61f6a85857749c531ceb314
rzvxa <rzvxa@protonmail.com>
2023-11-01 01:52:09 +0330

    Changed st font to RobotoMono

--- st-0.9/config.h
+++ st-0.9/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraMono Nerd Font:style=Regular:pixelsize=12:antialias=true:autohint=true";
+// static char *font = "FiraMono Nerd Font:style=Regular:pixelsize=12:antialias=true:autohint=true";
+static char *font = "RobotoMono Nerd Font Mono:style=Regular:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
